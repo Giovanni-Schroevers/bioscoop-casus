8f389551802f1b6a5bf6ed5db1aab1a16ad0aefc
raulvdzande <raulvdzande740@gmail.com>
2026-03-03 21:44:06 +0100

    added idea files for rider

--- BioscoopCasus.Models/obj/Debug/net10.0/BioscoopCasus.Models.AssemblyInfo.cs
+++ BioscoopCasus.Models/obj/Debug/net10.0/BioscoopCasus.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioscoopCasus.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbef43ba8c22ea26045964136027fdfb7e045dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioscoopCasus.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioscoopCasus.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
