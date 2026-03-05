1b17c4e2ad60ef8452b6bfff755f76eca10c9ae8
JasperRutte <jasperrutte@gmail.com>
2026-03-06 00:12:33 +0100

    add MoviesOverviewService and MoviesOverview.razor for frontend

--- BioscoopCasus.Web/Program.cs
+++ BioscoopCasus.Web/Program.cs
@@ -12,11 +12,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Point the HttpClient to our backend API (running on port 5064 for HTTP to avoid SSL issues)
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7181/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5064/") });
 
 // Register authentication & authorization components
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<MoviesOverviewService>();
 
 await builder.Build().RunAsync();
