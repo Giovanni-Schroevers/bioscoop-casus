86c4ab845e5ccda8fef81d7cb9a6f96542f3d0ad
JasperRutte <jasperrutte@gmail.com>
2026-03-08 21:00:56 +0100

    add qr payment simulation and a pin terminal simulation

--- BioscoopCasus.Web/Program.cs
+++ BioscoopCasus.Web/Program.cs
@@ -19,5 +19,6 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<MoviesOverviewService>();
+builder.Services.AddScoped<PaymentService>();
 
 await builder.Build().RunAsync();
