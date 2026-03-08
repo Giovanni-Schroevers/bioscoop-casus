996bae3b49090e95d5991d296179c19606ca1574
raulvdzande <raulvdzande740@gmail.com>
2026-03-08 19:34:16 +0100

    Made Working Automatic seat choice

--- BioscoopCasus.Web/Program.cs
+++ BioscoopCasus.Web/Program.cs
@@ -11,14 +11,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Point the HttpClient to our backend API (running on port 5064 for HTTP to avoid SSL issues)
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7181/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5064/") });
 
 // Register authentication & authorization components
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<ReservationService>();
+builder.Services.AddScoped<SeatSelectionService>();
 builder.Services.AddSingleton<QrCodeHelper>();
 
 await builder.Build().RunAsync();
