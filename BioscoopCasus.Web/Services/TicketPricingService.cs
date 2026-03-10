7f7010fb51ed2d974ee5e2951eb808c1206398fe
MDMoet <maicolmoet@gmail.com>
2026-03-10 13:18:36 +0100

    Ticket pricing backend and file

---
+++ BioscoopCasus.Web/Services/TicketPricingService.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using BioscoopCasus.Models.DataModels;
+
+namespace BioscoopCasus.Web.Services;
+
+public class TicketPricingService
+{
+    private TicketPricingConfig _pricingConfig = new();
+
+    public async Task InitializeAsync(HttpClient httpClient)
+    {
+        var json = await httpClient.GetStringAsync("ticketPricing");
+        var config = JsonSerializer.Deserialize<TicketPricingConfig>(json);
+
+        _pricingConfig = config ?? throw new InvalidOperationException("Ticket pricing configuration could not be loaded.");
+    }
+
+    public decimal GetBasePrice(int durationMinutes)
+    {
+        return durationMinutes > _pricingConfig.BasePrice.LongMovieThresholdMinutes ? _pricingConfig.BasePrice.LongMovie : _pricingConfig.BasePrice.Normal;
+    }
+
+    public bool IsStudentDiscountValid(DateTime showtime)
+    {
+        return _pricingConfig.Rules.StudentValidDays
+            .Contains((int)showtime.DayOfWeek);
+    }
+
+    public bool IsSeniorDiscountValid(DateTime showtime)
+    {
+        return _pricingConfig.Rules.SeniorValidDays
+            .Contains((int)showtime.DayOfWeek);
+    }
+
+    public bool IsChildDiscountValid(DateTime showtime)
+    {
+        return showtime.Hour < _pricingConfig.Rules.ChildBeforeHour;
+    }
+
+    public decimal GetChildDiscount() => _pricingConfig.Discounts.Child;
+    public decimal GetStudentDiscount() => _pricingConfig.Discounts.Student;
+    public decimal GetSeniorDiscount() => _pricingConfig.Discounts.Senior;
+
+    public decimal GetThreeDSurcharge() => _pricingConfig.Surcharges.ThreeD;
+
+    public bool IsVoucherValid(DateTime showtime)
+    {
+        var day = showtime.DayOfWeek;
+
+        var validWeekday =
+            day is >= DayOfWeek.Monday and <= DayOfWeek.Thursday;
+
+        return validWeekday;
+    }
+}
