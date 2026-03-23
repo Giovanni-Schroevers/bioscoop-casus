aff12ac3cbd82c4157277be9c2b0ca039855ba21
MDMoet <maicolmoet@gmail.com>
2026-03-23 20:48:16 +0100

    Email templates migration

---
+++ BioscoopCasus.API/Migrations/20260323194336_AddEmailTemplates.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BioscoopCasus.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailTemplates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                    name: "EmailTemplates",
+                    columns: table => new
+                    {
+                        Id = table.Column<int>(type: "int", nullable: false)
+                            .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                        Name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                            .Annotation("MySql:CharSet", "utf8mb4"),
+                        Subject = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: true)
+                            .Annotation("MySql:CharSet", "utf8mb4"),
+                        Body = table.Column<string>(type: "longtext", nullable: false)
+                            .Annotation("MySql:CharSet", "utf8mb4")
+                    },
+                    constraints: table =>
+                    {
+                        table.PrimaryKey("PK_EmailTemplates", x => x.Id);
+                    })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailTemplates_Name",
+                table: "EmailTemplates",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailTemplates");
+        }
+    }
+}
